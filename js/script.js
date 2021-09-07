1900eb8a1c8fac06762ce3430588ede1fa935bf8
Angelo7x <abennici96@gmail.com>
2021-09-07 17:29:12 +0200

    up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
+// Creare un oggetto studente
 var studente = {
-    "nome" : "Angelo",
-    "cognome" : "Bennici",
-    "eta" : 24
+    nome : "Angelo",
+    cognome : "Bennici",
+    eta : 24
+};
+
+// Creare un ciclo for-in e stampare
+
+for ( var key in studente) {
+    console.log(studente[key]);
 }
 
-console.log(studente);
+// Creare un array con oggetti studenti
+
+var studenti = [
+    {
+        nome : "Matteo",
+        cognome : "Bianchi",
+        eta : 27
+    },
+
+    {
+        nome : "Gabriele",
+        cognome : "Rossi",
+        eta : 29
+    },
+
+    {
+        nome : "Antonio",
+        cognome : "Verdi",
+        eta : 31
+    }
+];
+
+// Ciclo per stampare gli oggetti dentro l'array
+for ( var i = 0 ; i < studenti.length ; i++) {
+    console.log(studenti[i]);
+}
+
+// Crere i 3 prompt chiedendo all'utente di inserire nome, cognome e eta del nuovo studente e pusharlo dentro l'array
+
+studenti.push((prompt("Inserisci il nome dello studente")));
+studenti.push((prompt("Inserisci il cognome dello studente")));
+studenti.push((parseInt(prompt("Inserisci l'età dello studente"))));
